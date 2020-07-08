d5bf3cfcbd74087afff2f82fcd2802e5f80f1c88
manuel-arno-korfmann-webentwicklung <manuelkorfmann4@gmail.com>
2020-07-08 02:14:44 +0200

    ...

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+    class SessionsController < ApplicationController
+      def create
+        # @user = User.find_or_create_from_auth_hash(auth_hash)
+        # self.current_user = @user
+        session[:auth_hash] = auth_hash
+        redirect_to '/'
+      end
+
+      protected
+
+      def auth_hash
+        request.env['omniauth.auth']
+      end
+    end
+  end
+end
