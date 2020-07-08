3636acbb013ac49d0d75e1910596ee50fc40679e
manuel-arno-korfmann-webentwicklung <manuelkorfmann4@gmail.com>
2020-07-08 02:29:40 +0200

    ...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@
       def create
         # @user = User.find_or_create_from_auth_hash(auth_hash)
         # self.current_user = @user
-        session[:auth_hash] = auth_hash
+        session[:auth_hash] = auth_hash[:extra][:raw_info][:first_name]
         redirect_to '/'
       end
 
