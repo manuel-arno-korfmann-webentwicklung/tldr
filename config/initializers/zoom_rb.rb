a60ed210415663b63602029b3713f63d4b671daa
manuel-arno-korfmann-webentwicklung <manuelkorfmann4@gmail.com>
2020-11-13 19:11:18 +0000

    test

--- config/initializers/zoom_rb.rb
+++ config/initializers/zoom_rb.rb
@@ -1,4 +1,7 @@
 
+require 'zoom_rb'
+
+
 Zoom.configure do |c|
   c.api_key = Rails.application.credentials.zoom[:oauth][:app_key]
   c.api_secret = Rails.application.credentials.zoom[:oauth][:app_secret]
